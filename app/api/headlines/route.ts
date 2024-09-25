faba3f2129a372a0f958ed9683b23c4a475cba97
galenczk <galenczk@gmail.com>
2024-09-25 16:37:22 -0700

    Article titles appear on page

--- app/api/headlines/route.ts
+++ app/api/headlines/route.ts
@@ -4,10 +4,10 @@ import { NextResponse } from 'next/server';
 //export const dynamic = 'force-static'
 
 export async function GET() {
-    const apiKey: string = process.env.THE_NEWS_API_KEY;
+    const apiKey: string | undefined = process.env.THE_NEWS_API_KEY;
 
     const params = new URLSearchParams({
-        api_token: apiKey,
+        api_token: apiKey!,
         locale: 'us',
         sort: 'published_on',
         exclude_categories: 'sports, food, travel, entertainment',
--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,13 @@ export default async function Home() {
 
     let data = await fetch('http://localhost:3000/api/headlines')
     let articles = await data.json()
-    console.log(articles);
     
-    
-
     return (
         <div>
             
+            {articles.data.map((article, key) => (
+                <div id={key}>{article.title}</div>
+            ))}
             
         </div>
     );
